189416fc0cc8ceea7ad628f9b5aa0b12e43ce24b
Bice93 <bianca.cerulo@hotmail.it>
2022-06-23 16:10:19 +0200

    Add click Square/ click btn Play

--- js/main.js
+++ js/main.js
@@ -7,12 +7,22 @@
 
 
 const containerGrid = document.getElementById(`container_square`); //recupero il parent dei quadratini
+const buttonPlay = document.getElementById(`btn_play`); //recupero il bottone play
 
 for (let i=1; i < 101; i++){  
     const square = createSquare();
-    square.innerText = i;
+    square.innerText = i;  // aggiungo l'indice corrispondente al quadratino
     containerGrid.append(square); // aggiungo al parent il quadratino per 100 volte con il ciclo
+
+    square.addEventListener(`click`,function(){ // al click del quadratino coloro la casella di verde
+        square.classList.add(`active`);
+        console.log(`Hai cliccato la casella: ${i}`)
+    })
 }
+
+buttonPlay.addEventListener(`click`, function(){ //al click del bottone play visualizzo la griglia di gioco
+    containerGrid.classList.add(`look`);
+})
 
 
 
